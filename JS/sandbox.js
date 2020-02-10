2faad15731c9b70a2848667bf5d7f8f7d88d2270
Chaz <tkachukiam@gmail.com>
2020-02-10 20:25:36 +0200

    First commit

---
+++ JS/sandbox.js
@@ -0,0 +1,8 @@
+'use strict';
+
+
+
+let isChecked = true,
+    isClose = false;
+
+    console.log(isChecked || isClose);
---
+++ temperature.js
@@ -0,0 +1,17 @@
+/*
+the method should translate the temperature in degrees
+Celsius and return the value dergees Fahrenheit:
+public class Clazz {
+    public static void main(String[] args) {
+        System.out.println(celsToFahr(23));    
+    }
+    
+    public static double celsToFahr(int cels) {
+
+        return 0;
+
+    }
+}
+*/
+
+//Formula: T(F) = T(C) x 9/5 + 32
